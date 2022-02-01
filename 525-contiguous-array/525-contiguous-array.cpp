18e627664c827b0f9497a2f7f86c304899f19aee
rishabh9935 <72228974+rishabh9935@users.noreply.github.com>
2022-02-02 01:40:25 +0530

    Time: 274 ms (11.35%), Space: 83.8 MB (55.43%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -9,26 +9,26 @@ public:
         int start=0;
         int end=-1;
         int curr=0;
-        int sum=0;
         unordered_map <int , int> map;
         // map[0]=1;
         for(int i=0;i<nums.size();i++){
             curr+=nums[i];
-            if(curr==sum){
+            if(curr==0){
                 start=0;
                 end=i+1;
                 int x = end - start;
                 maxi=max(maxi,x);
             }
-            if(map.find(curr-sum)!=map.end()){
-                start=map[curr-sum];
+            if(map.find(curr)!=map.end()){
+                start=map[curr];
                 end=i;
                 int x = end - start;
                 maxi=max(maxi,x);
             }
             else{
-            map[curr]=i;
+                map[curr]=i;
             }
+            
         }
         return maxi;
     }
