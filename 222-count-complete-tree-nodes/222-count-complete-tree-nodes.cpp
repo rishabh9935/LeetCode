b8aeca9198e71dfa73f9d5c703756a28493705dc
rishabh9935 <72228974+rishabh9935@users.noreply.github.com>
2022-03-28 23:28:02 +0530

    Time: 48 ms (46.13%), Space: 30.8 MB (64.02%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -14,17 +14,9 @@ public:
     int countNodes(TreeNode* root) {
         if(root == NULL) return 0;
         int tNodes;
-        // int cLeft = 0 , cRight = 0;
-        // while(root){
-        //     cLeft++;
-        //     root = root->left;
-        // }
-        // while(root){
-        //     cRight++;
-        //     root = root->right;
-        // }
-        int cLeft = leftHeight(root);
-        int cRight = rightHeight(root);
+        int cLeft = 0 , cRight = 0;
+        cLeft = leftHeight(root);
+        cRight = rightHeight(root);
         if(cLeft == cRight){
             tNodes = pow(2,cLeft) - 1;
             return tNodes;
@@ -48,5 +40,36 @@ public:
             }
         return cRight;
     }
+    
+    // int countNodes(TreeNode* root) {
+    //     if(root == NULL) return 0;
+    //     int tNodes;
+    //     queue<TreeNode*> ql,qr;
+    //     ql.push(root);
+    //     qr.push(root);
+    //     int cLeft = 0 , cRight = 0;
+    //     while(!ql.empty()){
+    //         TreeNode* curr = ql.front();
+    //         ql.pop();
+    //         if(curr->left){
+    //             cLeft++;
+    //             ql.push(curr->left);
+    //         }
+    //     }
+    //     while(!qr.empty()){
+    //         TreeNode* curr = qr.front();
+    //         qr.pop();
+    //         if(curr->right){
+    //             cRight++;
+    //             qr.push(curr->right);
+    //         }
+    //     }
+    //     if(cLeft == cRight){
+    //         tNodes = pow(2,cLeft)-1;
+    //         return tNodes;
+    //     }
+    //     tNodes = 1+countNodes(root->left)+countNodes(root->right);
+    //     return tNodes;
+    // }
 
 };
