dbad4ee3c22ba303ef43f83f7ff127f2b9d39e79
rishabh9935 <72228974+rishabh9935@users.noreply.github.com>
2022-05-18 18:28:24 +0530

    Time: 11 ms (84.15%), Space: 9 MB (93.85%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -19,39 +19,38 @@ public:
         return dp[n][sum];
 	}
     int findTargetSumWays(vector<int>& nums, int target) {
-        // int total = 0;
-        // for(int i=0;i<nums.size();i++)
-        //     total+=nums[i];
-        // if(target>total||target<(-total))
-        //     return 0;
-        // if(total<target) return 0;
-        // int sum = (total-target)/2;
-        // return perfectSum(nums,nums.size(),sum);
-        int n=nums.size();
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-        }
+        int total = 0;
+        for(int i=0;i<nums.size();i++)
+            total+=nums[i];
+        total-=target;
+        if(total%2==1 || total<0) return 0;
+        int sum = total/2;
+        return perfectSum(nums,nums.size(),sum);
+//         int n=nums.size();
+//         int sum=0;
+//         for(int i=0;i<n;i++){
+//             sum+=nums[i];
+//         }
         
-        sum-=target;
-        if(sum%2==1 || sum<0)
-            return 0;
-        int w=sum/2;
-        vector<vector<int>>dp(n+1,vector<int>(w+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1;
-        }
-        for(int i=1;i<=w;i++){
-            dp[0][i]=0;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<=w;j++){
-                if(nums[i-1]<=j)
-                    dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]];
-                else
-                    dp[i][j]=dp[i-1][j];
-            }
-        }
-        return dp[n][w];
+//         sum-=target;
+//         if(sum%2==1 || sum<0)
+//             return 0;
+//         int w=sum/2;
+//         vector<vector<int>>dp(n+1,vector<int>(w+1,0));
+//         for(int i=0;i<=n;i++){
+//             dp[i][0]=1;
+//         }
+//         for(int i=1;i<=w;i++){
+//             dp[0][i]=0;
+//         }
+//         for(int i=1;i<=n;i++){
+//             for(int j=0;j<=w;j++){
+//                 if(nums[i-1]<=j)
+//                     dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]];
+//                 else
+//                     dp[i][j]=dp[i-1][j];
+//             }
+//         }
+//         return dp[n][w];
     }
 };
