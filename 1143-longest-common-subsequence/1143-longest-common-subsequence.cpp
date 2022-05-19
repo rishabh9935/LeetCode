418d0bd2cd883c97928ac8089fa96f7e516deaad
rishabh9935 <72228974+rishabh9935@users.noreply.github.com>
2022-05-20 02:16:27 +0530

    Time: 42 ms (26.27%), Space: 13.1 MB (19.57%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int LCS(string &X,string &Y,int n,int m,vector<vector<int>>&t){
-        if(n<0 || m<0)
+        if(n==0 || m==0)
             return 0;
         if(t[n][m]!=-1) return t[n][m];
-        if(X[n]==Y[m])
+        if(X[n-1]==Y[m-1])
             return t[n][m] = 1 + LCS(X,Y,n-1,m-1,t);
         else
             return t[n][m] = max(LCS(X,Y,n,m-1,t),LCS(X,Y,n-1,m,t));
@@ -13,8 +13,8 @@ public:
         // memset(t,-1,sizeof(t));
         int n = text1.size();
         int m = text2.size();
-        vector<vector<int>>t(n,vector<int>(m,-1));
-        return LCS(text1,text2,n-1,m-1,t);
+        vector<vector<int>>t(n+1,vector<int>(m+1,-1));
+        return LCS(text1,text2,n,m,t);
     }
     // int f(string &s1,string &s2, int idx1, int idx2, vector<vector<int>>&dp){
     //     if(idx1<0 || idx2<0) return 0;
