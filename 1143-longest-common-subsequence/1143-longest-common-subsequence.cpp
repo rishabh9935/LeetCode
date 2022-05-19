f9640998818cf4a29ea31bf73edcd631b75392fb
rishabh9935 <72228974+rishabh9935@users.noreply.github.com>
2022-05-20 02:28:12 +0530

    Time: 54 ms (13.91%), Space: 13.3 MB (10.08%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    
+    //Recursive + memoization
     int LCS(string &X,string &Y,int n,int m,vector<vector<int>>&t){
         if(n==0 || m==0)
             return 0;
@@ -16,20 +18,4 @@ public:
         vector<vector<int>>t(n+1,vector<int>(m+1,-1));
         return LCS(text1,text2,n,m,t);
     }
-    // int f(string &s1,string &s2, int idx1, int idx2, vector<vector<int>>&dp){
-    //     if(idx1<0 || idx2<0) return 0;
-    //     if(dp[idx1][idx2]!=-1){
-    //         return dp[idx1][idx2];
-    //     }
-    //     if(s1[idx1]==s2[idx2]){
-    //         return dp[idx1][idx2]= 1+f(s1,s2,idx1-1,idx2-1,dp);
-    //     }
-    //     return dp[idx1][idx2]=max(f(s1,s2,idx1-1,idx2,dp),f(s1,s2,idx1,idx2-1,dp));
-    // }
-    // int longestCommonSubsequence(string s1, string s2) {
-    //     int n=s1.size();
-    //     int m=s2.size();
-    //     vector<vector<int>>dp(n,vector<int>(m,-1));
-    //     return f(s1,s2,n-1,m-1,dp);
-    // }
 };
