a94d76f462dc885e8919821c1bbfa1af384fb511
rishabh9935 <72228974+rishabh9935@users.noreply.github.com>
2022-07-03 22:11:11 +0530

    Time: 4 ms (56.21%), Space: 7 MB (95.65%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        if(nums.size() < 2) return nums.size();
-        int i = 1; while(i < nums.size() && nums[i] == nums[i-1]) i++;
+        if(nums.size() < 2) return nums.size(); 
+        int i=1;
+        while(i<nums.size() && nums[i]==nums[i-1]){
+            i++;
+        }
         if(i == nums.size()) return 1;
-
-        int c = 2;
-        bool inc = nums[i] > nums[i-1];
-        while(i < nums.size()) {
-            if(inc) {
-                while(i < nums.size() && nums[i] >= nums[i-1]) i++;
-                if(i < nums.size()) c++; inc = false;
+        bool inc = nums[i] >= nums[i-1];
+        int c=2;
+        for(i=1;i<nums.size();i++){
+            if(inc){
+                while(i < nums.size() && nums[i] >= nums[i-1]){
+                    i++;
+                }
+                if(i < nums.size()) c++;
+                inc = false;
             }
-            else {
-                while(i < nums.size() && nums[i] <= nums[i-1]) i++;
-                if(i < nums.size()) c++; inc = true;
+            else{
+                while(i < nums.size() && nums[i] <= nums[i-1]){
+                    i++;
+                }
+                if(i < nums.size()) c++;
+                inc = true;
             }
-
         }
         return c;
     }
