5275fb88f65322bced5ba2476b6cdb9fee83ca68
rishabh9935 <72228974+rishabh9935@users.noreply.github.com>
2022-07-10 19:35:31 +0530

    Time: 56 ms (18.28%), Space: 13.3 MB (99.43%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    bool biper(int src, int color[], vector<vector<int>>& graph){
-        queue<int> q;
-        q.push(src);
-        color[src] = 1;
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            for(int i=0;i<graph[node].size();i++){
-                int it = graph[node][i];
-                if(color[it] == -1){
-                    color[it] = 1 - color[node];
-                    q.push(it);
-                }else if(color[it] == color[node]) return false;
-                else continue;
+    // bool biperbfs(int src, int color[], vector<vector<int>>& graph){
+    //     queue<int> q;
+    //     q.push(src);
+    //     color[src] = 1;
+    //     while(!q.empty()){
+    //         int node = q.front();
+    //         q.pop();
+    //         for(int i=0;i<graph[node].size();i++){
+    //             int it = graph[node][i];
+    //             if(color[it] == -1){
+    //                 color[it] = 1 - color[node];
+    //                 q.push(it);
+    //             }else if(color[it] == color[node]) return false;
+    //             else continue;
+    //         }
+    //     }
+    //     return true;
+    // }
+    
+    bool biperdfs(int node, vector<vector<int>>& graph, int color[]){
+        if(color[node] == -1) color[node] = 1;
+        for(int i=0;i<graph[node].size();i++){
+            int it = graph[node][i];
+            if(color[it] == -1){
+                color[it] = 1 - color[node];
+                if(!biperdfs(it, graph, color)) return false;
             }
+            else if(color[it] == color[node]) return false;
         }
         return true;
     }
@@ -24,7 +37,8 @@ public:
         memset(color, -1, sizeof color);
         for(int i=0;i<n;i++){
             if(color[i]==-1){
-                if(!biper(i, color, graph)) return false;
+                // if(!biperbfs(i, color, graph)) return false;
+                if(!biperdfs(i, graph, color)) return false;
             }
         }
         return true;
@@ -32,6 +46,5 @@ public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         return checkbipertite(graph, n);
-        
     }
 };
