8a141cacdf4ae90b190d6dd6073a8fe437d3eb20
rishabh9935 <72228974+rishabh9935@users.noreply.github.com>
2022-07-17 21:00:36 +0530

    Time: 4 ms (96.15%), Space: 6.6 MB (78.30%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,51 +1,24 @@
 class Solution {
 public:
-    int dp[55][55][55];
+    int dp[51][51][51];
+    int mod = 1000000007;
     
-    long long mod = 1e9 + 7;
-    
-    int dfs(int i, int j, int n, int m, int moves)
-    {
-        // base case, if we reached out of grid
+    long fun(int m, int n, int maxMove, int i, int j){
+        if(i>=m || i<0 || j<0 || j>=n) return 1;
+        if(maxMove<=0) return 0;
+        if(dp[i][j][maxMove] != -1) return dp[i][j][maxMove];
+        long res = 0;
+        res += fun(m, n, maxMove-1, i-1, j);
+        res += fun(m, n, maxMove-1, i+1, j);
+        res += fun(m, n, maxMove-1, i, j+1);
+        res += fun(m, n, maxMove-1, i, j-1);
+        // res = ((upR + downR + leftR + rightR)% mod);
+        return dp[i][j][maxMove] = res % mod;
         
-        if(i < 0 || i >= n || j < 0 || j >= m)
-            return 1;
-        
-        // if no moves are remaining
-        
-        if(moves <= 0)
-            return 0;
-        
-        // if already calculated
-        
-        if(dp[i][j][moves] != -1)
-            return dp[i][j][moves];
-        
-        // call for upper side
-        
-        int up = dfs(i - 1, j, n, m, moves - 1);
-        
-        // call for down side
-        
-        int down = dfs(i + 1, j, n, m, moves - 1);
-        
-        // call for left side
-        
-        int left = dfs(i, j - 1, n, m, moves - 1);
-        
-        
-        int right = dfs(i, j + 1, n, m, moves - 1);
-        
-        
-        return dp[i][j][moves] = (up % mod + down % mod + left % mod + right % mod) % mod;
     }
     
-    int findPaths(int n, int m, int maxMove, int startRow, int startColumn) {
-        
-        // initialize the dp with -1
-        
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
         memset(dp, -1, sizeof(dp));
-        
-        return dfs(startRow, startColumn, n, m, maxMove);
+        return fun(m, n, maxMove, startRow, startColumn) % mod;
     }
 };
