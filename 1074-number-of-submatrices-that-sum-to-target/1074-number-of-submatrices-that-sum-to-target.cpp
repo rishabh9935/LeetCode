acc52ae6f919fc5ff895e555e1974a0308dc004c
rishabh9935 <72228974+rishabh9935@users.noreply.github.com>
2022-07-19 23:26:53 +0530

    Time: 1185 ms (49.68%), Space: 161.1 MB (58.30%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
-        int rows = matrix.size() , cols = matrix[0].size();
-        
-        if(rows < 1){
-            return 0;
-        }
-        
-        // calculate prefix sum for rows
-        for(int row = 0 ; row < rows ; row++){
-            for(int col = 1 ; col < cols ;col++){
-                matrix[row][col] =  matrix[row][col] + matrix[row][col -1];
+    int subarraySum(vector<int>& nums, int k){
+        int n = nums.size();
+        int curr = 0;
+        int count = 0;
+        unordered_map<int , int> map;
+        // map[0]=1;  // we can also use the commented statement below instead of this
+        for(int i=0;i<n;i++){
+            curr+=nums[i];
+            if(curr-k==0){
+                count++;
             }
-        }
-      
-        int count = 0 , sum ;
-        unordered_map<int , int> counter;
-        for(int colstart = 0 ; colstart < cols ;colstart++){
-            for(int col = colstart ; col < cols; col++){
-                counter.clear();
-                counter[0] = 1;
-                sum =0;
-                for(int row = 0 ; row < rows ;row++){
-                    sum += matrix[row][col] - (colstart > 0 ? matrix[row][colstart - 1] : 0 );
-                    count += (counter.find(sum- target) != counter.end() ? counter[sum-target] : 0);
-                    counter[sum]++;
-                }
+            if(map.find(curr-k)!=map.end()){
+                count = count + map[curr-k];
             }
+            map[curr]++;
         }
         return count;
+    }
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        int ans = 0;
+        for(int i=0;i<m;i++){
+            vector<int> sumRow(n,0);
+            for(int j=i;j<m;j++){
+                for(int k=0;k<n;k++){
+                    sumRow[k] += matrix[j][k];
+                }
+                ans += subarraySum(sumRow, target);
+            }
+        }
+        return ans;
     }
 };
