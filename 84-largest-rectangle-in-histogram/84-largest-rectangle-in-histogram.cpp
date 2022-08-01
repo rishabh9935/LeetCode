372c3bad4a1471a94d4b154bcc42ab746a4d1582
rishabh9935 <72228974+rishabh9935@users.noreply.github.com>
2022-08-01 22:38:20 +0530

    Time: 433 ms (9.12%), Space: 93.6 MB (7.36%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,50 @@
+class Solution {
+
+    vector<int> findLeft(vector<int> arr, int n){
+        stack<int> s;
+        vector<int> ans(n);
+        s.push(-1);
+        for(int i=n-1;i>=0;i--){
+            int curr = arr[i];
+            while(s.top() != -1 && arr[s.top()] >= curr)
+                s.pop();
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+  
+    vector<int> findRight(vector<int> arr, int n){
+        stack<int> s;
+        vector<int> ans(n);
+        s.push(-1);
+        for(int i=0;i<n;i++){
+            int curr = arr[i];
+            while(s.top() != -1 && arr[s.top()] >= curr)
+                s.pop();
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+    public:
+    
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> left(n);
+        left = findRight(heights, n);
+        vector<int> right(n);
+        right = findLeft(heights, n);
+        int area = INT_MIN;
+        for(int i=0;i<n;i++){
+            if(right[i] == -1){
+                right[i] = n;
+            }
+            int l = heights[i];
+            int b = right[i] - left[i] - 1;
+            int s = l*b;
+            area = max(area, s);
+        }
+        return area;
+    }
+};
