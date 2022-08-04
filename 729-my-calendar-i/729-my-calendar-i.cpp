343f5c14403a46b8b46e9aa2101bb5f8b42ade8c
rishabh9935 <72228974+rishabh9935@users.noreply.github.com>
2022-08-05 03:53:39 +0530

    Time: 135 ms (70.82%), Space: 37.7 MB (87.88%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,19 +1,17 @@
 class MyCalendar {
 public:
-    vector<pair<int, int>> booking;
-	
+    vector<pair<int, int>> v;
     MyCalendar() {
         
     }
+    
     bool book(int start, int end) {
-        for (pair<int, int> temp : booking)
-        {
-            if (max(temp.first, start) < min(end, temp.second))
-            {
+        for(auto it : v){
+            if(it.first<end && start<it.second){
                 return false;
-            } 
+            }
         }
-        booking.push_back({start, end});
+        v.push_back({start, end});
         return true;
     }
 };
